f670436cb829b56bbc8f195d8e22d5f67d29e5a3
onnoo <sweyjw@gmail.com>
2021-07-18 07:11:55 +0900

    version 1.0.0

---
+++ history.js
@@ -0,0 +1,12 @@
+document.querySelectorAll(".js-wiki-history-revision").forEach((elem, index) => {
+    var revision = elem.querySelector("div.text-mono").innerText;
+    var pTag = elem.querySelector("div p.color-text-primary");
+    var aTag = document.createElement("a");
+    aTag.innerText = pTag.innerText;
+    aTag.classList.add("h5");
+    aTag.classList.add("mb-1");
+    aTag.classList.add("color-text-primary");
+    aTag.href = window.location.pathname.replace("_history", "_compare/") + revision; 
+    
+    pTag.parentElement.replaceChild(aTag, pTag);
+});
