c62d2e403852bb81240e690a5d5944dda10c166d
onnoo <sweyjw@gmail.com>
2022-01-31 05:25:49 +0900

    fix: encode URL of wiki page

--- compare.js
+++ compare.js
@@ -1,5 +1,6 @@
 var aTag = document.querySelector(".Link--primary");
 var title = aTag.title.replace(".md", "");
+title = encodeURIComponent(title)
 var href = document.location.href;
 var idx = href.indexOf("_compare");
 
